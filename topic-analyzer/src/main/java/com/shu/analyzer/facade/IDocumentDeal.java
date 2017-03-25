06b750c9b383e8025ccc6bde086cd657e2f0142d
Lichangya <1749362086@qq.com>
2017-03-25 23:43:47 +0800

    分词模块引入dao层依赖

---
+++ topic-analyzer/src/main/java/com/shu/analyzer/facade/IDocumentDeal.java
@@ -0,0 +1,9 @@
+package com.shu.analyzer.facade;
+
+/**
+ * Description：文本处理类
+ * User:Lichangya
+ * Date:2017/3/25.
+ */
+public interface IDocumentDeal {
+}
---
+++ topic-analyzer/src/main/java/com/shu/analyzer/impl/DocumentDeal.java
@@ -0,0 +1,13 @@
+package com.shu.analyzer.impl;
+
+import com.shu.analyzer.facade.IDocumentDeal;
+import org.springframework.stereotype.Component;
+
+/**
+ * Description：文本处理实现类
+ * User:Lichangya
+ * Date:2017/3/25.
+ */
+@Component
+public class DocumentDeal implements IDocumentDeal {
+}
